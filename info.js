f77615aec38081d2a35c9e63bed728e9bb6c7359
Davidson <davidson.rb100@gmail.com>
2023-03-17 00:54:27 -0300

    first commit

---
+++ info.js
@@ -0,0 +1,51 @@
+let itensJson = [
+    { id: 0, name: 'Hipopótamo', dataName: 'hippo' },
+    { id: 1, name: 'Peixe', dataName: 'fish' },
+    { id: 2, name: 'Coração', dataName: 'heart' },
+    { id: 3, name: 'Estrela', dataName: 'star' },
+    { id: 4, name: 'Sino', dataName: 'bell' },
+    { id: 5, name: 'Pena', dataName: 'feather' },
+    { id: 6, name: 'Dragão', dataName: 'dragon' },
+    { id: 7, name: 'Lontra', dataName: 'otter' },
+    { id: 8, name: 'Verme', dataName: 'worm' },
+    { id: 9, name: 'Lua', dataName: 'moon' },
+    { id: 10, name: 'Sol', dataName: 'sun' },
+    { id: 11, name: 'Folha', dataName: 'leaf' },
+    { id: 12, name: 'Arvore', dataName: 'tree' },
+    { id: 13, name: 'Fogo', dataName: 'fire' },
+    { id: 14, name: 'Carro', dataName: 'car' },
+    { id: 15, name: 'Aranha', dataName: 'spider' },
+    { id: 16, name: 'Camarão', dataName: 'shrimp' },
+    { id: 17, name: 'Caminhão', dataName: 'truck' },
+    { id: 18, name: 'Moto', dataName: 'motorcycle' },
+    { id: 19, name: 'Musica', dataName: 'music' },
+    { id: 20, name: 'Fantasma', dataName: 'ghost' },
+    { id: 21, name: 'Mosquito', dataName: 'mosquito' },
+    { id: 22, name: 'Chave', dataName: 'key' },
+    { id: 23, name: 'Cavalo', dataName: 'horse' },
+    { id: 24, name: 'Sapo', dataName: 'frog' },
+    { id: 25, name: 'Blusa', dataName: 'shirt' },
+    { id: 26, name: 'Helicóptero', dataName: 'helicopter' },
+    { id: 27, name: 'Cachorro', dataName: 'dog' },
+    { id: 28, name: 'Nuvem', dataName: 'cloud' },
+    { id: 29, name: 'Vaca', dataName: 'cow' },
+    { id: 30, name: 'Gato', dataName: 'cat' }
+]
+
+let cores = [
+    '#FF0000', // vermelho
+    '#FFA500', // laranja
+    '#FFFF00', // amarelo
+    '#008000', // verde
+    '#0000FF', // azul
+    '#4B0082', // índigo
+    '#EE82EE', // violeta
+    '#FFC0CB', // rosa claro
+    '#FF69B4', // rosa intenso
+    '#800000', // marrom escuro
+    '#808000', // verde oliva
+    '#008080', // azul petróleo
+    '#800080', // roxo
+    '#C0C0C0', // cinza claro
+    '#000FFF'
+];
---
+++ script.js
@@ -0,0 +1,168 @@
+
+let quantidade = 5;
+
+document.querySelector(".drag-area--item").innerHTML = '';
+document.querySelector(".drop-area--item").innerHTML = '';
+
+function criarTela(itensJson, quantidade) {
+    let nome = [];
+    let dataNames = [];
+
+    cores.sort(() => Math.random() - 0.5);
+
+    // Percorre o array de itensJson e adiciona os valores de id e dataName em arrays separados
+    itensJson.forEach((item) => {
+        nome.push(item.name);
+        dataNames.push(item.dataName);
+    });
+
+    let area_drag = document.querySelector(".drag-area--item");
+    let area_drop = document.querySelector(".drop-area--item");
+
+    for (let i = 0; i < quantidade; i++) {
+        // Seleciona um id e um dataName aleatoriamente dos arrays criados anteriormente
+        let randomIndex = Math.floor(Math.random() * nome.length);
+        let randomName = nome[randomIndex];
+        let randomDataName = dataNames[randomIndex];
+
+        // Seleciona uma cor aleatória da lista de cores randomizadas
+        let randomColor = cores[i % cores.length];
+
+        // Adiciona o ícone na tela com o id, dataName e cor aleatórios selecionados
+        area_drag.innerHTML += `<i data-name="${randomName}" data-color="${randomColor}"
+        class=" fa-solid fa-${randomDataName} fa-4x drag-item" draggable="true"
+        style="display:flex; color: ${randomColor}; order:${Math.random().toFixed(0)}"></i>`;
+
+        // Adiciona o ícone na tela com o id e dataName aleatórios selecionados
+        area_drop.innerHTML += `<div data-name="${randomName}" class="drop-item"><span>${randomName}</span></div>`;
+    }
+}
+
+
+//  BOTOES 
+document.querySelector('.btn--qtmenos').addEventListener('click', () => {
+    if (quantidade > 5) {
+        quantidade--;
+        document.querySelector('.qt').innerHTML = quantidade;
+    }
+});
+
+document.querySelector('.btn--qtmais').addEventListener('click', () => {
+    if (quantidade < 20) {
+        quantidade++;
+        document.querySelector('.qt').innerHTML = quantidade;
+    }
+
+});
+
+// Btn-start
+document.querySelector('.start').addEventListener('click', () => {
+    let areaWidth = document.querySelector('.area').clientWidth;
+    document.querySelector('.area').style.marginLeft = `-${areaWidth}px`;
+
+    criarTela(itensJson, quantidade);
+
+
+    document.querySelectorAll('.drag-item').forEach(drag => {
+        drag.addEventListener('dragstart', dragStart);
+        drag.addEventListener('dragend', dragEnd);
+    });
+
+    document.querySelectorAll('.drop-item').forEach(drop => {
+        drop.addEventListener('dragover', dragOver);
+        drop.addEventListener('dragleave', dragLeave);
+        drop.addEventListener('drop', dropItem);
+    });
+
+
+    document.querySelector('.drag-area--item').addEventListener('dragover', dragOverNeutral);
+    document.querySelector('.drag-area--item').addEventListener('dragleave', dragLeaveNeutral);
+    document.querySelector('.drag-area--item').addEventListener('drop', dropNeutral);
+
+});
+
+
+// Functions Item
+function dragStart(e) {
+    e.currentTarget.classList.add('dragging');
+}
+
+function dragEnd(e) {
+    e.currentTarget.classList.remove('dragging');
+}
+
+// Functions Area
+function dragOver(e) {
+    if (e.currentTarget.querySelector('.drag-item') === null) {
+        e.preventDefault();
+        e.currentTarget.classList.add('hover');
+    }
+}
+
+function dragLeave(e) {
+    e.currentTarget.classList.remove('hover');
+}
+
+function dropItem(e) {
+    e.currentTarget.classList.remove('hover');
+
+    if (e.currentTarget.querySelector('.drag-item') === null) {
+        let dragItem = document.querySelector('.drag-item.dragging');
+
+        e.currentTarget.innerHTML = '';
+        e.currentTarget.style.backgroundColor = dragItem.getAttribute('data-color');
+        dragItem.style.color = '#FFF';
+
+        e.currentTarget.appendChild(dragItem);
+        updatesAreas();
+    }
+}
+
+
+// Functions Neutral Area
+
+function dragOverNeutral(e) {
+    e.preventDefault();
+    e.currentTarget.classList.add('hover');
+}
+
+function dragLeaveNeutral(e) {
+    e.currentTarget.classList.remove('hover');
+}
+
+function dropNeutral(e) {
+    e.currentTarget.classList.remove('hover');
+    let dragItem = document.querySelector('.drag-item.dragging');
+    dragItem.style.color = dragItem.getAttribute('data-color');
+    e.currentTarget.appendChild(dragItem);
+
+    document.querySelectorAll('.drop-item').forEach(drop => {
+        if (drop.innerHTML === '') {
+            drop.style.backgroundColor = '#FFF';
+            drop.innerHTML = `<span>${drop.getAttribute('data-name')}</span>`;
+            drop.classList.remove('correct');
+        }
+    });
+    updatesAreas();
+}
+
+//  Logic Function
+function updatesAreas() {
+    let dragItem = document.querySelector('.drag-item.dragging');
+
+    console.log(dragItem.getAttribute('data-name'))
+
+
+    document.querySelectorAll('.drop-item').forEach(drop => {
+
+        if (dragItem.getAttribute('data-name') === drop.getAttribute('data-name')) {
+            console.log(drop.getAttribute('data-name'))
+            drop.classList.add('correct');
+        }
+        else {
+            drop.classList.remove('correct');
+        }
+    });
+
+
+}
